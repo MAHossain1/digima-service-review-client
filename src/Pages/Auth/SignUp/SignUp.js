a544b9a5fa96e4c5ae1027871be52e5dd561f60a
Md. Arman Hossain <mahpro110@gmail.com>
2023-03-08 17:59:17 +0600

    Login and sign up page created

--- src/Pages/Auth/SignUp/SignUp.js
+++ src/Pages/Auth/SignUp/SignUp.js
@@ -1,9 +1,73 @@
 import React from "react";
+import { FaFacebook } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { GoMarkGithub } from "react-icons/go";
+import { Link } from "react-router-dom";
+import img from "../../../assests/login/login.svg";
 
 const SignUp = () => {
   return (
-    <div>
-      <h2>Sign Up Now</h2>
+    <div className="hero w-full my-20">
+      <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+        <div className="text-center lg:text-left">
+          <img className="w-3/4" src={img} alt="" />
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-12">
+          <h1 className="text-5xl text-center font-bold">Sign Up</h1>
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <a href="/" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <input className="btn btn-error" type="submit" value="Sign Up" />
+            </div>
+          </form>
+          <p className="text-center font-semibold text-slate-700">
+            Or Sign Up with
+          </p>
+          <div className="text-center my-4 text-3xl">
+            <button>
+              <FaFacebook className="text-blue-600" />
+            </button>
+            <button>
+              <FcGoogle className="ml-4" />
+            </button>
+            <button className="ml-4">
+              <GoMarkGithub />
+            </button>
+          </div>
+          <p className="text-center">
+            Have an Account?{" "}
+            <Link className="font-bold text-orange-600" to="/login">
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
